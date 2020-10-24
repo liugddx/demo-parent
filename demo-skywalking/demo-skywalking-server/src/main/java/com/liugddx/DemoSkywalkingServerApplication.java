8cadf2bd8df0012617c9f8297cc73414801a4237
liugd <liugd@si-tech.com.cn>
2020-10-24 15:40:54 +0800

    first commit

---
+++ demo-skywalking/demo-skywalking-server/src/main/java/com/liugddx/DemoSkywalkingServerApplication.java
@@ -0,0 +1,13 @@
+package com.liugddx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSkywalkingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSkywalkingServerApplication.class, args);
+    }
+
+}
